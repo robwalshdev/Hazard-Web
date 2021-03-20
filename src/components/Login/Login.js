9ae4c8f221c8ead8bdad2c41ec7b250c298327b7
Robert Walsh <robwalshdev@gmail.com>
2021-03-20 23:05:38 +0000

    User auth & Updated UI
    Signed-off-by: Robert Walsh <robwalshdev@gmail.com>

---
+++ src/components/Login/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './Login.css';
+import Map from "../Map/Map";
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:5000/auth', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+}
+
+export default function Login({ setToken }) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
+    return (
+        <div className="Login">
+            <Map />
+            <div className="LoginSection">
+                <form onSubmit={handleSubmit} className="LoginForm">
+                    <h2>Login</h2>
+                    <h5>Please enter your username and password to login.</h5>
+                    <input type="text" placeholder="Username" className="LoginInput" onChange={e => setUserName(e.target.value)} />
+                    <input type="password" placeholder="Password" className="LoginInput" onChange={e => setPassword(e.target.value)} />
+                    <button type="submit" className="LoginButton">Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+};
--- src/components/StatisticCard/StatisticCard.js
+++ src/components/StatisticCard/StatisticCard.js
@@ -1,11 +1,11 @@
 import { React } from "react";
 import "./StatisticCard.css";
 
-const StatisticCard = () => {
+const StatisticCard = ({ statValue, statTitle }) => {
     return (
         <div className="StatisticCard">
-            <h1>10</h1>
-            <p>Total number of hazards</p>
+            <h1>{statValue}</h1>
+            <p>{statTitle}</p>
         </div>
     );
 }
